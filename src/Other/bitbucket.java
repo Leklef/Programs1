455ec72c1ebb98d078589ccf2871f9817c835bf3
LekLef <lenar8553@gmail.com>
2016-02-12 16:39:01 +0300

    bitbucket

---
+++ src/Other/bitbucket.java
@@ -0,0 +1,10 @@
+package Other;
+
+/**
+ * Created by lenar on 12.02.2016.
+ */
+public class bitbucket {
+    public static void main(String[] args){
+        System.out.print("Bitbucket");
+    }
+}
